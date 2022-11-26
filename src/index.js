592826907a1d32e9f17792c59dab3610c1692b8c
Nikola Mirilo <nikolamirilo@gmail.com>
2022-11-26 12:16:01 +0100

    deleted contexts

--- src/index.js
+++ src/index.js
@@ -1,21 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
-import { GlobalStateProvider } from "./context/GlobalContext";
-import { ImagesContextProvider } from "./context/ImagesContext";
-import { ThemeContextProvider } from "./context/ThemeContext";
 import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router>
-    <GlobalStateProvider>
-      <ThemeContextProvider>
-        <ImagesContextProvider>
-          <App />
-        </ImagesContextProvider>
-      </ThemeContextProvider>
-    </GlobalStateProvider>
-  </Router>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
